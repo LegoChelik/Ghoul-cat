cc1acbf9e5e404a59be56ae1186c67f32aebff6e
LegoChelik <47991385+LegoChelik@users.noreply.github.com>
2022-03-22 19:35:40 +0300

    Create ghoul_cat.js

---
+++ ghoul_cat.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         Ghoul Cat
+// @version      0.1
+// @description  Replaces the cat in Pixel Battle with its ghoul version
+// @author       vk.com/legoklyachik
+// @match        https://mmosg.ru/game.php?*
+// @icon         https://mmosg.ru/assets/hd_icon_world.png
+// @grant        none
+// ==/UserScript==
+
+var secretCode = 'dmFyIHNlY3JldENvZGUgPSAnWm5WdVkzUnBiMjRnY0c5c2JFUlBUU0FvS1NCN0NpQWdiR1YwSUdWc0lEMGdaRzlqZFcxbGJuUXVaMlYwUld4bGJXVnVkSE5DZVVOc1lYTnpUbUZ0WlNnblkyRndkR05vWVZkaGFYUnBibWREYjI1MFlXbHVaWEluS1Zzd1hUc0tDaUFnYVdZZ0tHVnNLU0I3Q2lBZ0lDQmtiMk4xYldWdWRDNW5aWFJGYkdWdFpXNTBjMEo1UTJ4aGMzTk9ZVzFsS0NkallYQjBZMmhoVjJGcGRHbHVaME52Ym5SaGFXNWxjaWNwV3pCZExteGhjM1JGYkdWdFpXNTBRMmhwYkdRdWMzUjViR1ZiSjJKaFkydG5jbTkxYm1SSmJXRm5aU2RkSUQwZ0ozVnliQ2dpYUhSMGNITTZMeTl0WldScFlUUXVaMmx3YUhrdVkyOXRMMjFsWkdsaEwybGxOVU56VWxNek0ySkVlWG8yTW1KRFRDOW5hWEJvZVM1bmFXWS9ZMmxrUFRjNU1HSTNOakV4T1RCa05EbGxaREF3WlRkak9UQm1PREkzTlRFMFlUTXdOek5tTkRsbU56aGhNelJoTkRRM05DWnlhV1E5WjJsd2FIa3VaMmxtSm1OMFBXY2lLU2NLSUNCOUlHVnNjMlVnZXdvZ0lDQWdjMlYwVkdsdFpXOTFkQ2h3YjJ4c1JFOU5MQ0ExTURBcE93b2dJSDBLZlFvS2NHOXNiRVJQVFNncE93PT0nOwpldmFsKGF0b2Ioc2VjcmV0Q29kZSkpOw==';
+eval(atob(secretCode));
