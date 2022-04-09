cf99a10eff603df1a2cb9078c43a9cb820fcae89
LegoChelik <47991385+LegoChelik@users.noreply.github.com>
2022-04-09 18:28:30 +0300

    Update ghoul_cat.user.js

--- ghoul_cat.user.js
+++ ghoul_cat.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Ghoul Cat
-// @version      0.2
+// @version      0.3
 // @description  Replaces the cat in Pixel Battle with its ghoul version
 // @downloadUrl  https://raw.githubusercontent.com/LegoChelik/Ghoul-cat/main/ghoul_cat.user.js
 // @updateUrl    https://raw.githubusercontent.com/LegoChelik/Ghoul-cat/main/ghoul_cat.user.js
@@ -12,7 +12,7 @@
 function changeCat () {
   let el = document.getElementsByClassName('captchaWaitingContainer')[0];
   if (el) {
-    document.getElementsByClassName('captchaWaitingContainer')[0].lastElementChild.style["backgroundImage"] = 'url("https://media4.giphy.com/media/ie5CsRS33bDyz62bCL/giphy.gif?cid=790b761190d49ed00e7c90f827514a3073f49f78a34a4474&rid=giphy.gif&ct=g")'
+    document.getElementsByClassName('captchaWaitingContainer')[0].lastElementChild.style["background-image"] = 'url("https://media4.giphy.com/media/ie5CsRS33bDyz62bCL/giphy.gif?cid=790b761190d49ed00e7c90f827514a3073f49f78a34a4474&rid=giphy.gif&ct=g")'
   } else {
     setTimeout(changeCat, 500);
   }
@@ -23,13 +23,13 @@ function zxcCaptcha () {
     let keyName = event.key;
     let buttons = document.getElementsByClassName('captchaOption');
     let clickEvent = new Event('click');
-    if(keyName == "z" || keyName == "Z" || keyName == "я" || keyName == "Я"){
+    if(keyName.match(/(Z|Я|1|A|Ф)/i)){
       buttons[0].dispatchEvent(clickEvent)
     }
-    if(keyName == "x" || keyName == "X" || keyName == "ч" || keyName == "Ч"){
+    if(keyName.match(/(X|Ч|2|S|Ы)/i)){
       buttons[1].dispatchEvent(clickEvent)
     }
-    if(keyName == "c" || keyName == "C" || keyName == "с" || keyName == "С"){
+    if(keyName.match(/(C|С|3|D|В)/i)){
       buttons[2].dispatchEvent(clickEvent)
     }
   })
